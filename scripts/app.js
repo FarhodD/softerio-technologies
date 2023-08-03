cf8e06f768cda1bf4c96bbd92713e99662ec80cc
FarhodD <farhoddavlatov0120@gmail.com>
2023-08-03 19:33:50 +0500

    main section and fixed some bugs

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".burger__menu"),
+  submenu = document.querySelector(".menu");
+
+function toggleMenu() {
+  menuBtn.classList.toggle("active");
+  submenu.classList.toggle("vis__submenu");
+}
